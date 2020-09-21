2963070a99b0eb176ef9498723b7128b2cc082ba
Christopher Harrison <ch12@sanger.ac.uk>
2020-09-21 12:44:36 +0100

    Bash is not Python

--- user.sh
+++ user.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-'''
-Software will not work without the required Access and Secret Key
-Access key and Secret key can be found at: /nfs/users/nfs_m/mercury/.s3cfg
-These should be entered into the appropriate variables
-'''
+# Software will not work without the required Access and Secret Key
+# Access key and Secret key can be found at: /nfs/users/nfs_m/mercury/.s3cfg
+# These should be entered into the appropriate variables
 export S3_AWS_ACCESS_KEY=""
 export S3_AWS_SECRET_KEY=""
 export S3_SANGER_URL="https://cog.sanger.ac.uk"
